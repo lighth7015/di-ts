4b7e482c8899e6b18dcccde0b50a5ecfa03d1a80
Robert Butler <me@r-butler.net>
2020-04-23 12:42:20 -0400

    Create ServiceDecorator.js

---
+++ ServiceDecorator.js
@@ -0,0 +1,6 @@
+import { GenericClassDecorator, Type } from "./Util";
+/**
+ * @returns {GenericClassDecorator<Type<any>>}
+ * @constructor
+ */
+export declare const Service: () => GenericClassDecorator<Type<any>>;
